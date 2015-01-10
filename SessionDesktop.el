45c3f7dd1b70ce2e78c31453eba58c66998a9e27
Thomas Heavey <thomasjheavey@gmail.com>
2015-01-10 12:49:35 -0500

    Data interpretation Mathematica notebook
    To interpret and look at the output of the code, a Mathematica notebook
    was composed to make videos of the process as well as plots of some
    important variables as a function of the steps taken.

---
+++ SessionDesktop.el
@@ -0,0 +1,78 @@
+;; -*- mode: emacs-lisp; coding: emacs-mule; -*-
+;; --------------------------------------------------------------------------
+;; Desktop File for Emacs
+;; --------------------------------------------------------------------------
+;; Created Fri Jan  9 15:36:29 2015
+;; Desktop file format version 206
+;; Emacs version 24.4.51.2
+(setq revive:frame-configuration-to-restore
+  '(progn 
+     (revive:restore-frame '((tool-bar-position . top) (explicit-name) (icon-name) (bottom-divider-width . 0) (right-divider-width . 0) (top . 23) (left . 720) (unsplittable) (modeline . t) (width . 98) (height . 47) (fringe) (mouse-color . "black") (environment) (visibility . t) (cursor-color . "Red") (background-mode . light) (horizontal-scroll-bars . t) (fullscreen) (alpha) (scroll-bar-width . 15) (cursor-type . box) (auto-lower) (auto-raise) (icon-type) (title) (buffer-predicate) (tool-bar-lines . 1) (menu-bar-lines . 1) (right-fringe . 11) (left-fringe . 3) (line-spacing) (background-color . "White") (foreground-color . "Black") (vertical-scroll-bars . right) (internal-border-width . 0) (border-width . 0) (font . "-*-Monaco-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1") (fontsize . 0)) '(98 47 ((0 0 102 46)) (("~/Documents/BU/CH_655_Stat_Mech/New_Final_Project/lattice-reaction-model/mclatticecellobject.py" "mclatticecellobject.py" 16823 14669 ((tab "initializelat.py" "/Users/Tommy/Documents/BU/CH_655_Stat_Mech/New_Final_Project/lattice-reaction-model/initializelat.py") (tab "runlat.py" "/Users/Tommy/Documents/BU/CH_655_Stat_Mech/New_Final_Project/lattice-reaction-model/runlat.py") (tab "mclatticecellobject.py" "/Users/Tommy/Documents/BU/CH_655_Stat_Mech/New_Final_Project/lattice-reaction-model/mclatticecellobject.py"))))))
+     (revive:restore-frame '((tool-bar-position . top) (explicit-name) (icon-name) (bottom-divider-width . 0) (right-divider-width . 0) (top . 28) (left . 5) (unsplittable) (modeline . t) (width . 97) (height . 43) (visibility . t) (cursor-color . "Red") (background-mode . light) (fringe) (fullscreen) (alpha) (scroll-bar-width . 15) (cursor-type . box) (auto-lower) (auto-raise) (icon-type) (title) (buffer-predicate) (tool-bar-lines . 1) (menu-bar-lines . 1) (right-fringe . 11) (left-fringe . 3) (line-spacing) (background-color . "White") (foreground-color . "Black") (vertical-scroll-bars . right) (internal-border-width . 0) (border-width . 0) (font . "-*-Monaco-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1") (fontsize . 0)) '(97 43 ((0 0 101 42)) (("~/Documents/BU/CH_655_Stat_Mech/New_Final_Project/lattice-reaction-model/latticecellobject.py" "latticecellobject.py" 12826 12826 ((tab "latticecellobject.py" "/Users/Tommy/Documents/BU/CH_655_Stat_Mech/New_Final_Project/lattice-reaction-model/latticecellobject.py"))))))))
+
+;; Global section:
+(setq desktop-saved-frameset [frameset 1 (21680 15309 657405 0) (desktop . "206") "Tommy@air" nil nil ((((font-backend mac-ct ns) (fontsize . 0) (font . "-*-Monaco-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1") (border-width . 0) (internal-border-width . 0) (vertical-scroll-bars . right) (foreground-color . "Black") (background-color . "White") (line-spacing) (left-fringe . 3) (right-fringe . 11) (menu-bar-lines . 1) (tool-bar-lines . 1) (title) (icon-type) (auto-raise) (auto-lower) (cursor-type . box) (scroll-bar-width . 15) (alpha) (fullscreen) (horizontal-scroll-bars . t) (display-type . color) (background-mode . light) (cursor-color . "Red") (visibility . t) (environment) (mouse-color . "black") (fringe) (frameset--id . "6F9B-E5EA-9A3E-14A2") (frameset--mini t . t) (height . 47) (width . 98) (modeline . t) (minibuffer . t) (unsplittable) (left . 720) (top . 23) (right-divider-width . 0) (bottom-divider-width . 0) (icon-name) (display . "air") (explicit-name) (tool-bar-position . top)) ((min-height . 4) (min-width . 10) (min-height-ignore . 4) (min-width-ignore . 7) (min-height-safe . 1) (min-width-safe . 2) (min-pixel-height . 64) (min-pixel-width . 70) (min-pixel-height-ignore . 51) (min-pixel-width-ignore . 43) (min-pixel-height-safe . 16) (min-pixel-width-safe . 14)) leaf (pixel-width . 715) (pixel-height . 736) (total-width . 102) (total-height . 46) (normal-height . 1.0) (normal-width . 1.0) (buffer "mclatticecellobject.py" (selected . t) (hscroll . 0) (fringes 3 11 nil) (margins 4) (scroll-bars 15 3 t nil) (vscroll . 0) (dedicated) (point . 16823) (start . 14669))) (((font-backend mac-ct ns) (fontsize . 0) (font . "-*-Monaco-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1") (border-width . 0) (internal-border-width . 0) (vertical-scroll-bars . right) (foreground-color . "Black") (background-color . "White") (line-spacing) (left-fringe . 3) (right-fringe . 11) (menu-bar-lines . 1) (tool-bar-lines . 1) (title) (icon-type) (auto-raise) (auto-lower) (cursor-type . box) (scroll-bar-width . 15) (alpha) (fullscreen) (fringe) (display-type . color) (background-mode . light) (cursor-color . "Red") (visibility . t) (frameset--id . "CF9F-E283-9AA9-D324") (frameset--mini t) (height . 43) (width . 97) (modeline . t) (minibuffer . t) (unsplittable) (left . 5) (top . 28) (right-divider-width . 0) (bottom-divider-width . 0) (icon-name) (display . "air") (explicit-name) (tool-bar-position . top)) ((min-height . 4) (min-width . 10) (min-height-ignore . 2) (min-width-ignore . 7) (min-height-safe . 1) (min-width-safe . 2) (min-pixel-height . 64) (min-pixel-width . 70) (min-pixel-height-ignore . 32) (min-pixel-width-ignore . 43) (min-pixel-height-safe . 16) (min-pixel-width-safe . 14)) leaf (pixel-width . 708) (pixel-height . 672) (total-width . 101) (total-height . 42) (normal-height . 1.0) (normal-width . 1.0) (buffer "latticecellobject.py" (selected) (hscroll . 0) (fringes 3 11 nil) (margins 4) (scroll-bars 15 3 t nil) (vscroll . 0) (dedicated) (point . 12826) (start . 12826))))])
+(setq desktop-missing-file-warning nil)
+(setq tags-file-name nil)
+(setq tags-table-list nil)
+(setq search-ring '("check" "move_" "rando" "dimensions" "dim" "dimensions" "molep" "mol" "maxt" "nthick" "meshabs"))
+(setq regexp-search-ring nil)
+(setq register-alist nil)
+(setq file-name-history '("~/Documents/BU/CH_655_Stat_Mech/New_Final_Project/lattice-reaction-model/runlat.py" "~/Documents/BU/CH_655_Stat_Mech/New_Final_Project/lattice-reaction-model/mclatticecellobject.py" "~/Documents/BU/CH_655_Stat_Mech/New_Final_Project/lattice-reaction-model/runlat.py" "~/Documents/BU/CH_655_Stat_Mech/New_Final_Project/lattice-reaction-model/mclatticecellobject.py" "~/Documents/BU/CH_655_Stat_Mech/New_Final_Project/lattice-reaction-model/runlat.py" "~/Documents/BU/CH_655_Stat_Mech/New_Final_Project/lattice-reaction-model/mclatticecellobject.py" "~/Documents/BU/CH_655_Stat_Mech/New_Final_Project/lattice-reaction-model/runlat.py" "~/Documents/BU/CH_655_Stat_Mech/New_Final_Project/lattice-reaction-model/mclatticecellobject.py" "~/Documents/BU/CH_655_Stat_Mech/New_Final_Project/lattice-reaction-model/runlat.py" "~/Documents/BU/CH_655_Stat_Mech/New_Final_Project/lattice-reaction-model/mclatticecellobject.py" "~/Documents/BU/CH_655_Stat_Mech/New_Final_Project/lattice-reaction-model/runlat.py" "~/Documents/BU/CH_655_Stat_Mech/New_Final_Project/lattice-reaction-model/mclatticecellobject.py" "~/Documents/BU/CH_655_Stat_Mech/New_Final_Project/lattice-reaction-model/runlat.py" "~/Documents/BU/CH_655_Stat_Mech/New_Final_Project/lattice-reaction-model/mclatticecellobject.py" "~/Documents/BU/CH_655_Stat_Mech/New_Final_Project/lattice-reaction-model/runlat.py" "~/Documents/BU/CH_655_Stat_Mech/New_Final_Project/lattice-reaction-model/mclatticecellobject.py" "~/Documents/BU/CH_655_Stat_Mech/New_Final_Project/lattice-reaction-model/runlat.py" "~/Documents/BU/CH_655_Stat_Mech/New_Final_Project/lattice-reaction-model/initializelat.py" "~/Documents/BU/CH_655_Stat_Mech/New_Final_Project/lattice-reaction-model/mclatticecellobject.py" "~/Documents/BU/CH_655_Stat_Mech/New_Final_Project/lattice-reaction-model/runlat.py" "~/Documents/BU/CH_655_Stat_Mech/New_Final_Project/lattice-reaction-model/initializelat.py" "~/Documents/BU/CH_655_Stat_Mech/New_Final_Project/lattice-reaction-model/mclatticecellobject.py" "~/Documents/BU/CH_655_Stat_Mech/New_Final_Project/lattice-reaction-model/initializelat.py" "~/Documents/BU/CH_655_Stat_Mech/New_Final_Project/lattice-reaction-model/mclatticecellobject.py" "~/Documents/BU/CH_655_Stat_Mech/New_Final_Project/lattice-reaction-model/initializelat.py" "~/Documents/BU/CH_655_Stat_Mech/New_Final_Project/lattice-reaction-model/mclatticecellobject.py" "~/Documents/BU/CH_655_Stat_Mech/New_Final_Project/lattice-reaction-model/latticecellobject.py" "~/Documents/BU/CH_655_Stat_Mech/New_Final_Project/lattice-reaction-model/runlat.py" "~/Documents/BU/CH_655_Stat_Mech/New_Final_Project/lattice-reaction-model/initializelat.py" "~/Documents/BU/CH_655_Stat_Mech/New_Final_Project/lattice-reaction-model/runlat.py" "~/Documents/BU/CH_655_Stat_Mech/New_Final_Project/lattice-reaction-model/latticecellobject.py" "~/Documents/BU/CH_655_Stat_Mech/New_Final_Project/lattice-reaction-model/mclatticecellobject.py" "~/Documents/Caltech/Junior Year/Junior Year Spring/CS 11 Python/lab 5/automaton_display.py" "~/Documents/BU/CH_655_Stat_Mech/New_Final_Project/lattice-reaction-model/testenv.py" "~/Documents/Caltech/Junior Year/Junior Year Spring/CS 11 Python/lab 5/automaton.py" "~/Documents/Caltech/Junior Year/Junior Year Spring/CS 11 Python/python_style_check" "/Library/Python/2.7/site-packages/pychecker" "~/Documents/Caltech/Junior Year/Junior Year Spring/CS 11 Python/lab 2/hanoi.py" "~/Documents/Caltech/Junior Year/Junior Year Spring/CS 11 Python/lab 1/hello" "~/Documents/Caltech/Junior Year/Junior Year Spring/CS 11 Python/lab 1/solve_quadratic_equation" "~/Desktop/th-taut3htmf.out" "~/Documents/BU/Coker Research Group/Porco Collab/th-3htmfjob"))
+
+;; Buffer section -- buffers listed in same order as in buffer list:
+(desktop-create-buffer 206
+  "/Users/Tommy/Documents/BU/CH_655_Stat_Mech/New_Final_Project/lattice-reaction-model/latticecellobject.py"
+  "latticecellobject.py"
+  'python-mode
+  '(linum-mode outline-minor-mode)
+  12826
+  '(11649 nil)
+  nil
+  nil
+  '((tab-width . 4) (indent-tabs-mode) (buffer-file-coding-system . undecided-unix)))
+
+(desktop-create-buffer 206
+  "/Users/Tommy/Documents/BU/CH_655_Stat_Mech/New_Final_Project/lattice-reaction-model/mclatticecellobject.py"
+  "mclatticecellobject.py"
+  'python-mode
+  '(linum-mode outline-minor-mode)
+  16823
+  '(16581 nil)
+  nil
+  nil
+  '((tab-width . 4) (indent-tabs-mode) (buffer-file-coding-system . undecided-unix)))
+
+(desktop-create-buffer 206
+  "/Users/Tommy/Documents/BU/CH_655_Stat_Mech/New_Final_Project/lattice-reaction-model/runlat.py"
+  "runlat.py"
+  'python-mode
+  '(linum-mode outline-minor-mode)
+  607
+  '(605 nil)
+  nil
+  nil
+  '((tab-width . 4) (indent-tabs-mode) (buffer-file-coding-system . undecided-unix)))
+
+(desktop-create-buffer 206
+  "/Users/Tommy/Documents/BU/CH_655_Stat_Mech/New_Final_Project/lattice-reaction-model/initializelat.py"
+  "initializelat.py"
+  'python-mode
+  '(linum-mode outline-minor-mode)
+  1723
+  '(7708 nil)
+  nil
+  nil
+  '((tab-width . 4) (indent-tabs-mode) (buffer-file-coding-system . undecided-unix)))
+
+(desktop-create-buffer 206
+  "/Users/Tommy/Library/Application Support/Aquamacs Emacs/scratch buffer"
+  "*scratch*"
+  'text-mode
+  '(visual-line-mode smart-spacing-mode linum-mode)
+  1
+  '(nil nil)
+  nil
+  nil
+  '((buffer-file-coding-system . utf-8) (truncate-lines)))
+
